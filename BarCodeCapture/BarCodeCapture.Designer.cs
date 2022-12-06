be91deda9ae288d4deda5e61a80848fce8e3cd5d
williamanjo <william.anjodaguarda20@gmail.com>
2022-12-06 12:13:43 -0300

    Adicionar arquivos de projeto.

---
+++ BarCodeCapture/BarCodeCapture.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace BarCodeCapture
+{
+    partial class BarCodeCapture
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BarCodeCapture));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(972, 154);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(909, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Capture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(890, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // BarCodeCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 214);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BarCodeCapture";
+            this.Text = "BarCodeCapture";
+            this.Load += new System.EventHandler(this.BarCodeCapture_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ BarCodeCapture/BarCodeCapture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZXing;
+
+namespace BarCodeCapture
+{
+    public partial class BarCodeCapture : Form
+    {
+        public BarCodeCapture()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            if (Clipboard.ContainsImage())
+            {
+
+                pictureBox1.Image = Clipboard.GetImage();
+                BarcodeReader reader = new BarcodeReader();
+                var result = reader.Decode((Bitmap)pictureBox1.Image);
+                if (result != null) { 
+                    textBox1.Text = result.ToString(); 
+                }
+            }
+            else {
+                MessageBox.Show("Utilize a Combinação (Win + Shift + S) para capturar o Codigo de Barra");
+            }
+        }
+
+        private void BarCodeCapture_Load(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsImage())
+            {
+
+                pictureBox1.Image = Clipboard.GetImage();
+                BarcodeReader reader = new BarcodeReader();
+                var result = reader.Decode((Bitmap)pictureBox1.Image);
+                if (result != null)
+                {
+                    textBox1.Text = result.ToString();
+                }
+            }
+        }
+    }
+}
